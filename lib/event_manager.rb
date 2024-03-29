2d27b9390c0e3b3ae900907d8d114aa2a8146c02
NathanD <nathand50444@gmail.com>
2024-03-29 23:00:55 +0000

    method created to standardise all zipcodes
    'clean_zipcode' method allows all zipcodes to be 5 digits in length.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,14 @@
 require 'csv'
 
+    # Data cleanup necessary using 'if' + 'elsif' conditionals.
+    # If the zipcode contains exactly five digits, then it is okay to continue.
+    # If the zipcode contains more than 5 digits, it can be trimmed to the first five digits.
+    # If the zipcode contains less than 5 digits, add zeros to the beginning until it is 5 digits.
+
+def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open('event_attendees.csv', 
@@ -9,6 +18,7 @@ header_converters: :symbol
 
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
+    zipcode = clean_zipcode(row[:zipcode])
     puts "#{name} #{zipcode}"
 end
+
